8d7a6fcf789dce997c8531e059054cf2f5351efc
Brian Thomason <brian.r.thomason@gmail.com>
2015-09-24 00:27:00 -0400

    Include Program.cs

--- SMITEAPI/Program.cs
+++ SMITEAPI/Program.cs
@@ -16,10 +16,10 @@ namespace SMITEAPI
     {
         static void Main(string[] args)
         {
-            APICalls.SerializationPath = @"G:\JSONDebugging";
+            APICall.SerializationPath = @"G:\JSONDebugging";
             APISession session = null;
             //APIPlayer player = APICalls.APICall<APIPlayer[]>(APICalls.Call.GetPlayer, APICalls.ReturnMethod.JSON, ref session, "ybadragon").First();
-            var objreturn = APICalls.APICall<object>(APICalls.Call.GetEsportsProLeagueDetails, APICalls.ReturnMethod.JSON, ref session);
+            var objreturn = APICall.Call<APIESportsProLeagueDetail>(APICall.CallType.GetEsportsProLeagueDetails, APICall.ReturnMethod.JSON, ref session);
             ConsoleColor c = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Press any key to close.");
