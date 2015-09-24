ba1d27c0669ff071ca01db41c6e1ec28c7fab309
Brian Thomason <brian.r.thomason@gmail.com>
2015-09-24 00:29:52 -0400

    Fix Program.cs for next commit. From now on Exclude Program.cs from commits.

--- SMITEAPI/Program.cs
+++ SMITEAPI/Program.cs
@@ -16,13 +16,13 @@ namespace SMITEAPI
     {
         static void Main(string[] args)
         {
-            APICalls.SerializationPath = @"G:\JSONDebugging";
-            APICalls.SerializationPath = @"C:\Users\Chris\Desktop\MyTestingFolder";
+            APICall.SerializationPath = @"G:\JSONDebugging";
+            APICall.SerializationPath = @"C:\Users\Chris\Desktop\MyTestingFolder";
             APISession session = null;
-            APIPlayer player = APICalls.APICall<APIPlayer[]>(APICalls.Call.GetPlayer, APICalls.ReturnMethod.JSON, ref session, "ybadragon").First();
-            var objreturn = APICalls.APICall<APITopMatch[]>(APICalls.Call.GetTopMatches, APICalls.ReturnMethod.JSON, ref session);
+            APIPlayer player = APICall.Call<APIPlayer[]>(APICall.CallType.GetPlayer, APICall.ReturnMethod.JSON, ref session, "ybadragon").First();
+            var objreturn1 = APICall.Call<APITopMatch[]>(APICall.CallType.GetTopMatches, APICall.ReturnMethod.JSON, ref session);
             //APIPlayer player = APICalls.APICall<APIPlayer[]>(APICalls.Call.GetPlayer, APICalls.ReturnMethod.JSON, ref session, "ybadragon").First();
-            var objreturn = APICall.Call<APIESportsProLeagueDetail>(APICall.CallType.GetEsportsProLeagueDetails, APICall.ReturnMethod.JSON, ref session);
+            var objreturn2 = APICall.Call<APIESportsProLeagueDetail>(APICall.CallType.GetEsportsProLeagueDetails, APICall.ReturnMethod.JSON, ref session);
             ConsoleColor c = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Press any key to close.");
