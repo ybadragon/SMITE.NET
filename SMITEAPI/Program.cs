5b9f6302569ab2612e1d65abf8835badbbbb1c69
Brian Thomason <brian.r.thomason@gmail.com>
2016-01-15 23:39:35 -0500

    Test Commit

--- SMITEAPI/Program.cs
+++ SMITEAPI/Program.cs
@@ -16,6 +16,8 @@ namespace SMITEAPI
     {
         static void Main(string[] args)
         {
+
+            int DEVID = 0;
             APICall.Initialize(DEVID, "AUTHKEY", @"DEBUG_LOG_LOCATION", "http://api.smitegame.com/smiteapi.svc/");
             APISession session = null;
             APIGodInfo[] gods = APICall.Call<APIGodInfo[]>(APICall.CallType.GetGods, APICall.ReturnMethod.JSON, ref session, ((int)APICall.LanguageCode.English).ToString());
